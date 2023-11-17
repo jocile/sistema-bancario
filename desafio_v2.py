2061b86401fe6e5a10c90454042dfee247211350
jocile <jocile@gmail.com>
2023-11-16 21:01:14 -0300

    :sparkles: Atualizados os parâmetros das funções

--- desafio_v2.py
+++ desafio_v2.py
@@ -12,11 +12,12 @@ def depositar(saldo, extrato):
     if valor > 0:
         saldo += valor
         extrato.append(f"Depósito: R$ {valor:.2f}")
-        return saldo
+        print(f"Realizado o depósito de R$ {valor:.2f}")
     else:
         print("Valor de depósito inválido!")
+    return saldo
 
-def sacar(saldo, extrato, limite, LIMITE_SAQUES, numero_saques):
+def sacar(*, saldo, extrato, limite, LIMITE_SAQUES, numero_saques):
     valor = float(input("Informe o valor do saque: "))
     if valor > saldo:
         print("Operação falhou! Você não tem saldo suficiente.")
@@ -27,12 +28,13 @@ def sacar(saldo, extrato, limite, LIMITE_SAQUES, numero_saques):
     elif valor > 0:
         saldo -= valor
         extrato.append(f"Saque: R$ {valor:.2f}")
-        numero_saques += 1        
+        numero_saques += 1
+        print(f"Realizado o saque de R$ {valor:.2f}") 
     else:
         print("Operação falhou! O valor informado é inválido.")
     return saldo, numero_saques
 
-def mostrar(extrato, saldo):
+def mostrar(saldo, /, *, extrato):
     print("======== EXTRATO ========")
     if len(extrato) == 0:
         print('Não foram realizadas movimentações.')
@@ -41,6 +43,12 @@ def mostrar(extrato, saldo):
             print(f"{indice+1}: {movimentacao}")
         print(f'\nSaldo atual: R$ {saldo:.2f}')                
     print("===============================")
+
+def criar_usuario():
+    pass
+
+def criar_conta_corrente():
+    pass
 
 def main():    
     saldo = 0
@@ -54,9 +62,9 @@ def main():
         if opcao == "d":
             saldo = depositar(saldo, extrato)
         elif opcao == "s":
-            saldo, numero_saques = sacar(saldo, extrato, limite, LIMITE_SAQUES, numero_saques)
+            saldo, numero_saques = sacar(saldo=saldo, extrato=extrato, limite=limite, LIMITE_SAQUES=LIMITE_SAQUES, numero_saques=numero_saques)
         elif opcao == "e":
-            mostrar(extrato, saldo)
+            mostrar(saldo, extrato = extrato)
         elif opcao == "x":
             break
     else:
